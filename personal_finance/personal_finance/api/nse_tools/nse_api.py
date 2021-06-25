ab90d43f4031fc5a318841b565daf72e08399fd8
Aditya Duggal <adityaduggal@gmail.com>
2021-06-25 10:42:43 +0530

    feat: Added the ability to get the Stock Prices Historical

--- personal_finance/personal_finance/api/nse_tools/nse_api.py
+++ personal_finance/personal_finance/api/nse_tools/nse_api.py
@@ -3,12 +3,20 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import frappe
 from nsetools import Nse
+from nsepy import get_history
+nse = Nse()
 
 
 def get_code_list(type):
-    nse = Nse()
     if type == "Stock":
         return nse.get_stock_codes()
     elif type == "Index":
         return nse.get_index_list()
+
+
+def get_stock_quote(stock, frm_dt, to_dt):
+    q = get_history(symbol=stock, start=frm_dt, end=to_dt)
+    q = q.fillna(0)
+    return q
